d28fb39feced4b8379d9abf76114091830192c4d
Gustav Laestander <lagu16@nti.umea>
2018-10-02 10:12:07 +0200

    update

--- ruterspader2.py
+++ ruterspader2.py
@@ -1,1 +1,18 @@
-Fråga = int(input("Vilket kort vill du"))
+hand = []
+print("Finns i sjön!")
+fråga = 0
+while fråga != 5:
+    try:
+        fråga = int(input("Vilket kort vill du fråga efter (1 -4) "))
+    except:
+        print("Skriv ett nummer, försök igen")
+    import random
+    aikort = random.randint(1,4)
+    print(aikort)
+    if fråga == aikort:   
+        hand.append(aikort)
+        print("i din hand har du", hand)
+    else:
+        print("finns i sjön")
+else:
+    print("Tack för spelet")
