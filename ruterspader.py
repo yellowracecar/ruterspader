07050a6532a3fb87b54b6a3640a9d02a90f28d89
Gustav Laestander <lagu16@nti.umea>
2018-10-02 08:44:19 +0200

    ruterspader2

--- ruterspader.py
+++ ruterspader.py
@@ -1,12 +1,23 @@
 run = 0
 while run == 0:
-    gissa = int(input("Gissa ett nummer mellan 1 - 4 "))
+    try: 
+        gissa = int(input("Gissa ett nummer mellan 1 - 4 "))
+    except:
+        print("Du måste välja en siffra")
     import random
     a = random.randint(1,4)
-    if gissa == a:
-        print("rätt")
-        run = 1
-    else:
-        print("fel")
+    if a = 1
+        1 = Hjärter
+    if a = 2
+        2 = Ruter
+    if a = 3
+        3 = Spader
+    if a = 4
+        4 = Klöver
+        if gissa == a:
+            print("rätt")
+            run = 1
+        else:
+            print("fel")
 else:
     print("Tack för spelet")
